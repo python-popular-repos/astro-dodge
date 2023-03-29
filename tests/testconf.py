501f8fd901d77b4d0bac835f7a9127e8880073ce
Kevin <kev.chu.pro@gmail.com>
2023-03-28 22:47:33 -0700

    test: add base pytest fixture for unit testing

--- tests/testconf.py
+++ tests/testconf.py
@@ -1,3 +1,9 @@
 import pytest
+from app.models import User
+
 
 # Fixtures
+@pytest.fixture(scope="module")
+def new_user():
+    user = User("test@pytest.com", "FlaskIsAwesome")
+    return user
---
+++ tests/unit/test_models.py
@@ -0,0 +1,44 @@
+"""
+This file (test_models.py) contains the unit tests for the models.py file.
+"""
+from app.models import User
+
+
+def test_new_user():
+    """
+    GIVEN a User model
+    WHEN a new User is created
+    THEN check the email, password_hashed, authenticated, and active fields are defined correctly
+    """
+    user = User("test@pytest.com", "FlaskIsAwesome")
+    assert user.email == "test@pytest.com"
+    assert user.password_hashed != "FlaskIsAwesome"
+    assert user.__repr__() == "<User: test@pytest.com>"
+    assert user.is_authenticated
+    assert user.is_active
+    assert not user.is_anonymous
+
+
+# def test_setting_password(new_user):
+#     """
+#     GIVEN an existing User
+#     WHEN the password for the user is set
+#     THEN check the password is stored correctly and not as plaintext
+#     """
+#     new_user.set_password("MyNewPassword")
+#     assert new_user.password_hashed != "MyNewPassword"
+#     assert new_user.is_password_correct("MyNewPassword")
+#     assert not new_user.is_password_correct("MyNewPassword2")
+#     assert not new_user.is_password_correct("FlaskIsAwesome")
+
+
+# def test_user_id(new_user):
+#     """
+#     GIVEN an existing User
+#     WHEN the ID of the user is defined to a value
+#     THEN check the user ID returns a string (and not an integer) as needed by Flask-WTF
+#     """
+#     new_user.id = 66
+#     assert isinstance(new_user.get_id(), str)
+#     assert not isinstance(new_user.get_id(), int)
+#     assert new_user.get_id() == "66"
