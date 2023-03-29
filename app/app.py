c957ff8d8e02f5d97300a1da4ba0ef09e5df13f1
Kevin <kev.chu.pro@gmail.com>
2023-03-28 22:47:02 -0700

    style: remove artifacts from namespace

--- app/app.py
+++ app/app.py
@@ -1,38 +1,20 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from sqlalchemy import DateTime
-import nasa
+from models import db
+import nasa as info
 
 app = Flask(__name__)
-data = nasa.format()
 
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "postgresql://postgres:password@localhost:5432/mydatabase"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy()
 db.init_app(app)
 
 
 @app.before_first_request
 def create_tables():
-    print("Hopefully Creating Tables")
     db.create_all()
-
-
-class User(db.Model):
-    __tablename__ = "astro_users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String)
-    created_at = db.Column(DateTime, nullable=False, default=datetime.utcnow)
-
-
-class Record(db.Model):
-    __tablename__ = "astro_record"
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(DateTime, nullable=False, default=datetime.utcnow)
 
 
 @app.route("/")
@@ -42,23 +24,29 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 @app.route("/list")
 def space_list():
+    data = info.format()
     return render_template("list.html", space_list=data, title="Space List")
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", title="Login")
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html", title="Register")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title="Profile")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8005, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
